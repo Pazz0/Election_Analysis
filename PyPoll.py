7eaaff3c82c180d29e53cf102f6d858d601b9a18
Massimo Scanga <pazzo@Massimos-MacBook-Pro.local>
2022-12-15 16:35:02 -0500

    added pypoll and other files

--- PyPoll.py
+++ PyPoll.py
@@ -5,21 +5,20 @@
 #4 total num of votes for ea candidate
 #5 Winner of election baased on popular vote
 
-# Import the datetime class from the datetime module.
-import datetime as dt
-# Use the now() attribute on the datetime class to get the present time.
-now = dt.datetime.now()
-# Print the present time.
-print("The time right now is ", now)
-
 import csv
 import os
 
 # Assign a variable for the file to load and the path.
-file_to_load = os.path.join("Resources", "election_results.csv")
+file_to_load = os.path.join("/Users/pazzo/Desktop/Analasys Projects Git Hub/Election_Analysis/Election_Analysis/Resources", "election_results.csv")
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("/Users/pazzo/Desktop/Analasys Projects Git Hub/Election_Analysis/Election_Analysis/analysis", "election_analysis.txt")
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
 
-     # To do: perform analysis.
-     print(election_data)
+    # Read the file object with the reader function.
+     file_reader = csv.reader(election_data)
+
+     # Print each row in the CSV file.
+     headers = next(file_reader)
+     print(headers)
